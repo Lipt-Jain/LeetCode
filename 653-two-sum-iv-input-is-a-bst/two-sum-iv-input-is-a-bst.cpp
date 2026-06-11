1c661d5e053c3dafd2f1ad553d6f6ba41a1cc435
liptjain <liptjaindhn@gmail.com>
2026-06-11 20:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 37.4 MB (48.82%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,42 +1,62 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class BSTIterator {
+    stack<TreeNode*> myStack;
+    bool reverse = true;
+
+public:
+    BSTIterator(TreeNode* root, bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    int next() {
+        TreeNode* tmpNode = myStack.top();
+        myStack.pop();
+
+        if (!reverse)
+            pushAll(tmpNode->right);
+        else
+            pushAll(tmpNode->left);
+
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode* node) {
+        while (node != NULL) {
+            myStack.push(node);
+
+            if (reverse)
+                node = node->right;
+            else
+                node = node->left;
+        }
+    }
+};
+
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int> & arr){
-        if(root==NULL){
-            return;
-        }
-        inorder(root->left,arr);
-        arr.push_back(root->val);
-        inorder(root->right,arr);
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int> arr;
-        inorder(root,arr);
-        int l=0;
-        int h=arr.size()-1;
-        while(l<h){
-            if(arr[l]+arr[h]==k){
+        if (!root) return false;
+
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int i = l.next();
+        int j = r.next();
+
+        while (i < j) {
+            if (i + j == k)
                 return true;
-            }
-            else{
-                if(arr[l]+arr[h]>k){
-                    h--;
-                }
-                else{
-                    l++;
-                }
-            }
+            else if (i + j < k)
+                i = l.next();
+            else
+                j = r.next();
         }
+
         return false;
     }
 };
