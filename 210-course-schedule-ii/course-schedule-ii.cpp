74a525612a0cf647f2e015f1c79d4f6a6aca08cd
liptjain <liptjaindhn@gmail.com>
2026-06-22 23:22:02 +0530

    Time: 4 ms (54.95%) | Memory: 19.1 MB (19.83%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+
+        vector<vector<int>> adj(n);
+
+        for (auto it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> d(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (auto it : adj[i]) {
+                d[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < n; i++) {
+            if (d[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> topo;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            topo.push_back(node);
+
+            for (auto it : adj[node]) {
+                d[it]--;
+                if (d[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        if (topo.size() == n) {
+            return topo;
+        }
+
+        return {};
+    }
+};
