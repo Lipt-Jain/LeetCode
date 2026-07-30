ddad36f92c57326a25749fcfe0ab3466b6b8a35d
liptjain <liptjaindhn@gmail.com>
2026-07-30 21:13:33 +0530

    Time: 3 ms (61.41%) | Memory: 18.5 MB (48.04%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,42 +8,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    ListNode* mergeTwoSortedLists(ListNode* l1,ListNode* l2){
-        if(l1==NULL){
-            return l2;
+    struct cmp {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
         }
-        if(l2==NULL){
-            return l1;
-        }
-        if(l1->val<=l2->val){
-            l1->next=mergeTwoSortedLists(l1->next,l2);
-            return l1;
-        }
-        else{
-            l2->next=mergeTwoSortedLists(l1,l2->next);
-            return l2;
-        }
-        return NULL;
-    }
-    ListNode* partitionAndMerge(int start,int end,vector<ListNode*>& lists){
-        if(start==end){
-            return lists[start];
-        }
-        if(start>end){
-            return NULL;
-        }
-        int mid= start+ (end-start)/2;
-        ListNode* L1=partitionAndMerge(start,mid,lists);
-        ListNode* L2=partitionAndMerge(mid+1,end,lists);
-        return mergeTwoSortedLists(L1,L2);
-    }
+    };
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int k=lists.size();
-        if(k==0){
-            return NULL;
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+
+        for (auto node : lists) {
+            if (node)
+                pq.push(node);
         }
-        return partitionAndMerge(0,k-1,lists);
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* curr = pq.top();
+            pq.pop();
+
+            tail->next = curr;
+            tail = curr;
+
+            if (curr->next)
+                pq.push(curr->next);
+        }
+
+        return dummy.next;
     }
 };
