5fb610170fbb41f038752a2b51a6bccfa538ba28
liptjain <liptjaindhn@gmail.com>
2026-08-08 15:35:10 +0530

    Time: 1 ms (21.94%) | Memory: 7.9 MB (48.85%) - LeetSync

--- 755-reach-a-number/reach-a-number.cpp
+++ 755-reach-a-number/reach-a-number.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int reachNumber(int target) {
-        int n=1;
-        int sum=1;
-        if(target==0 || target==1 || target==-1){
-            return abs(target);
-        }
-        while(abs(target)>sum){
+        target = abs(target);
+
+        int n = 0;
+        int sum = 0;
+
+        while (sum < target || (sum - target) % 2 != 0) {
             n++;
-            sum=(n*(n+1))/2;
-            if(abs(target)==sum){
-                return n;
-            }
+            sum += n;
         }
-        while((sum-abs(target))%2!=0){
-            n++;
-            sum=(n*(n+1))/2;
-        }
+
         return n;
     }
 };
